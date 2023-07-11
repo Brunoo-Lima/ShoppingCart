7b6a1b8580d7a3bc92470f95d035e0e1424930d5
Bruno Lima <bruno2.jean.lima@outlook.com>
2023-07-11 20:00:28 -0300

    add header and components

---
+++ src/components/Cart/CartBtn.jsx
@@ -0,0 +1,11 @@
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+import React from 'react';
+
+const CartBtn = () => {
+  return (
+    <button className="btnCart" type="button">
+      <AiOutlineShoppingCart />
+    </button>
+  );
+};
+export default CartBtn;
---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import Search from '../components/form/Search';
+import CartBtn from './Cart/CartBtn';
+
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header-bg">
+      <div className="container header">
+        <h1 className="">myShoppingCart</h1>
+        <div className="form ">
+          <Search />
+        </div>
+
+        <nav>
+          <CartBtn />
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
